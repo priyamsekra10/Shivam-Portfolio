6526b0dd0bfd7bac5b5686d9a77ce0f229045935
vipul-bhatia <vipul0592bhatia@gmail.com>
2024-03-02 01:29:38 +0530

    .

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ output: "export", // comment this line while development
   assetPrefix: "/",
   exportTrailingSlash: true, // comment this line while development
   basePath: "",
-  // trailingSlash: true,
+  trailingSlash: true,
   swcMinify: true,
   images: {
     unoptimized: true
